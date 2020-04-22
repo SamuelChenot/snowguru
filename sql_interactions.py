4b7e13a1352d13eecd5cd8b1bd99edd73831ee23
Samuel Chenot <samuelchenot@gmail.com>
2020-04-21 21:29:01 -0400

    added sql_interactions.py functions

--- sql_interactions.py
+++ sql_interactions.py
@@ -25,7 +25,6 @@ def get_new_uid(latitude, longitude):
     return int(data)
 
 
-
 #Add a mountain to a user ID
 def add_mountain_to_user(UID, MID):
     db = MySQLdb.connect('localhost', # The Host
@@ -42,21 +41,21 @@ def add_mountain_to_user(UID, MID):
 
     print("Mountain ID: ", MID, "Toggled to UserID: ", UID)
 
+
 # Takes UserID as UID and boolean saying weter to return all mountains or user home mountains
-def get_mountains_hourly(UID, home_mountain):
+def get_mountains_daily_info(MID):
     db = MySQLdb.connect('localhost', # The Host
                          'BaseUser', # username
                          'password', # password
                          'mydb') # name of the data base
     cursor = db.cursor()
 
-    if home_mountain == False:
-        sql_string = 'SELECT * FROM Hourly'
 
-    if home_mountain == True:
-        sql_string = 'SELECT * FROM Hourly'
-        sql_string += ' INNER JOIN HomeMT ON Hourly.MID = HomeMT.MID'
-        sql_string += ' WHERE '+str(UID)+' = HomeMT.UID'
+    sql_string = 'SELECT Mountains.Name, Daily.* FROM Daily'
+    sql_string += ' INNER JOIN HomeMT ON Daily.MID = HomeMT.MID'
+    sql_string += ' INNER JOIN Mountains ON Daily.MID = Mountains.MID'
+    sql_string += ' WHERE '+str(MID)+' = Daily.MID'
+    sql_string += ' GROUP BY Daily.Day'
 
 
     cursor.execute(sql_string)
@@ -67,3 +66,86 @@ def get_mountains_hourly(UID, home_mountain):
 
     return data #Formated as a 2d tuple
 
+
+#Takes User ID and boolean, returns alphabetically
+def search_lex(UID, home):
+
+    db = MySQLdb.connect('localhost', # The Host
+                         'BaseUser', # username
+                         'password', # password
+                         'mydb') # name of the data base
+    cursor = db.cursor()
+
+    #SQL processing
+    if home == True: home_ = '1'
+    else: home_ = '0'
+
+    #SQL processing
+    cursor.callproc('searchLex',[str(UID), str(home_), ])
+    data = cursor.fetchall()
+
+    cursor.close()
+
+    return data
+
+
+def update_user_coordinates(UID, LAT, LON):
+
+    db = MySQLdb.connect('localhost', # The Host
+                         'BaseUser', # username
+                         'password', # password
+                         'mydb') # name of the data base
+    cursor = db.cursor()
+
+
+    #SQL processing
+    cursor.callproc('updateCoordinates',[str(UID), str(LAT), str(LON), ])
+
+    cursor.close()
+
+    print("Updated User Coordinates")
+
+
+# Takes UserID as UID and boolean saying weter to return all mountains or user home mountains
+def get_mountain_Names():
+    db = MySQLdb.connect('localhost', # The Host
+                         'BaseUser', # username
+                         'password', # password
+                         'mydb') # name of the data base
+    cursor = db.cursor()
+
+    sql_string = 'SELECT Name, Hourly.MID FROM Mountains'
+    sql_string += ' INNER JOIN Hourly On Mountains.MID = Hourly.MID'
+    sql_string += ' GROUP BY Mountains.Name;'
+
+    cursor.execute(sql_string)
+    data = cursor.fetchall()
+
+    db.commit()
+    cursor.close()
+
+    return data #Formated as a 2d tuple
+
+# Takes UserID as UID and boolean saying weter to return all mountains or user home mountains
+def get_user_list(UID):
+    db = MySQLdb.connect('localhost', # The Host
+                         'BaseUser', # username
+                         'password', # password
+                         'mydb') # name of the data base
+    cursor = db.cursor()
+
+
+    sql_string = 'SELECT Mountains.Name, Hourly.MID FROM Hourly'
+    sql_string += ' INNER JOIN HomeMT ON Hourly.MID = HomeMT.MID'
+    sql_string += ' INNER JOIN Mountains ON Hourly.MID = Mountains.MID'
+    sql_string += ' WHERE '+str(UID)+' = HomeMT.UID'
+    sql_string += ' GROUP BY Hourly.MID;'
+
+
+    cursor.execute(sql_string)
+    data = cursor.fetchall()
+
+    db.commit()
+    cursor.close()
+
+    return data #Formated as a 2d tuple
